dcecd8d7d5f3ff9a4bb8d18cdb5d86d5f3b9926d
Toby Qin <toby.qin@live.com>
2017-06-09 17:23:17 +0800

    define business.

--- testcube_client/business.py
+++ testcube_client/business.py
@@ -1,12 +1,15 @@
 def run(name, files, **kwargs):
+    """One step to save a run with multiple xml files."""
     pass
 
 
 def start_run(name, team, product, version=None):
+    """Will save current run id in config and print out."""
     pass
 
 
-def finish_run(id, files):
+def finish_run(run_id, files):
+    """Follow up step to save run info after starting a run."""
     pass
 
 
@@ -23,4 +26,12 @@ def get_or_create_product(name, version=None):
 
 
 def add_product(name, version):
+    pass
+
+
+def get_or_create_testcase(name, team, product):
+    pass
+
+
+def update_or_create_result(run_id, result):
     pass
