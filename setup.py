c63eb7a579dae12ccb69929602b4817467494ec0
Toby Qin <toby.qin@live.com>
2017-10-26 09:48:28 +0800

    update pkg stage.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     zip_safe=False,
     keywords='testcube, testcube-client, test platform, test client',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
